727dd6e8e7d5a2bdebdd8fb40c9a3dbb475c1eaa
Nikos Epping <nikos.epping@protonmail.com>
2021-10-25 17:59:41 +0200

    Add Radio app

---
+++ App6_3/app/src/main/java/de/nikosepping/radio/model/RadioStation.java
@@ -0,0 +1,34 @@
+package de.nikosepping.radio.model;
+
+import androidx.annotation.NonNull;
+
+public class RadioStation {
+
+    private final String name;
+    private final String websiteUrl;
+    private final String streamUrl;
+
+    public RadioStation(String name, String websiteUrl, String streamUrl) {
+        this.name = name;
+        this.websiteUrl = websiteUrl;
+        this.streamUrl = streamUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    public String getStreamUrl() {
+        return streamUrl;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
